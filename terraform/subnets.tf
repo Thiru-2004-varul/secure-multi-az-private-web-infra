9267470854adb2facfad3e1cc8267dc23a0a1d5c
Thiru-2004-varul <thiruvarulg9@gmail.com>
2026-01-04 23:58:35 -0500

    feat(network) add multi-az public and private subnet

---
+++ terraform/subnets.tf
@@ -0,0 +1,28 @@
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+resource "aws_subnet" "public" {
+  count                   = 2
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-${count.index + 1}"
+  }
+}
+
+
+resource "aws_subnet" "private" {
+  count             = 2
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index + 10)
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+
+  tags = {
+    Name = "Private-subnet-${count.index + 1}"
+  }
+
+}
