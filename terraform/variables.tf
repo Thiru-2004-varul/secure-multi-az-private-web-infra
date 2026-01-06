d7c499eafe2f4aeef0b2c67974f74e55e144275b
Thiru-2004-varul <thiruvarulg9@gmail.com>
2026-01-06 07:19:15 -0500

    region updated

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,7 +7,7 @@ variable "project_name" {
 variable "region" {
   description = "AWS region"
   type        = string
-  default     = "us-east-1"
+  default     = "ap-south-1"
 }
 
 variable "vpc_cidr" {
