44053107ae8d0577334d1ae4a7e5f96144540217
Thiru-2004-varul <thiruvarulg9@gmail.com>
2026-01-06 05:52:48 -0500

    Parameterize infrastructure using variables

---
+++ terraform/variables.tf
@@ -0,0 +1,50 @@
+variable "project_name" {
+  description = "Name of the project"
+  type        = string
+  default     = "secure-multi-az-web"
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDRs for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDRs for private subnets"
+  type        = list(string)
+  default     = ["10.0.101.0/24", "10.0.102.0/24"]
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "asg_min" {
+  type    = number
+  default = 2
+}
+
+variable "asg_desired" {
+  type    = number
+  default = 2
+}
+
+variable "asg_max" {
+  type    = number
+  default = 4
+}
